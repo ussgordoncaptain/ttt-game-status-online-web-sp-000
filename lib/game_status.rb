ebe3d83a76d1b3e4602b7e6f6c8d6572d739ebde
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-02 20:58:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,56 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+
+WIN_COMBINATIONS = [[0,1,2],[0,4,8], [0, 3, 6], [1,4,7], [2,4,6], [2,5,8], [3,4,5], [6,7,8]]
+
+def won?(board)
+  ii=0 
+  while(ii < 8)
+    win_candidate=WIN_COMBINATIONS[ii]
+    pos_1 = board[win_candidate[0]]
+    pos_2 = board[win_candidate[1]]
+    pos_3 = board[win_candidate[2]]
+    if(board[win_candidate[0]]==" ")
+      return false 
+    end 
+    if (pos_1== pos_2 && pos_1 == pos_2 && pos_2 == pos_3)
+      return win_candidate
+    end
+    ii+=1
+  end
+  return false 
+end
+
+def full?(board)
+  ii=0 
+  while (ii<9)
+    if (board[ii]== " ")
+      return false
+    end
+    ii += 1 
+  end
+  return true
+end
+
+def draw?(board)
+  if(full?(board)==false)
+    return false 
+  elsif(won?(board)==true)
+    return false
+  else 
+    return true
+end
+def over?(board)
+  if(full?(board))
+    return true 
+    elsif (won?(board))
+     return true 
+   else 
+     return false
+end 
+
+def winner(board)
+  board_state= won?(board)
+end
